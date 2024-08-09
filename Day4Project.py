8feabf8877e8b269c32d6ca672162aecd1ecdf89
ninjastar779 <neeldebnath2@gmail.com>
2024-08-09 11:13:25 -0400

    Added Day4Project.py

---
+++ Day4Project.py
@@ -0,0 +1,10 @@
+print("Welcome to the story maker!")
+
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+place = input("Where do you live?: ")
+animal = input("What is your favorite type of animal?: ")
+
+print("\nOnce upon a time," + "\n" + name + " was a " + age + " year old person living in " + place + ".")
+print(name + " loved " + animal.lower() + " and thought they were the coolest animal in the world!")
+print("The end.")

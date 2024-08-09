9a02821d92efb49918a290b5761360de00b067ef
ninjastar779 <neeldebnath2@gmail.com>
2024-08-09 11:10:10 -0400

    Added Day3

---
+++ Day3.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")  # asks for user name
+lunch = input("What is your favorite food?: ")  # asks for user favorite food
+
+print("Hello " + name + " your favorite food is " + lunch) # prints user name and favorite food
+#concatenation

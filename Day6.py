9c2307a68376e7a920a0a8e3d0b2bc472857f969
ninjastar779 <neeldebnath2@gmail.com>
2024-08-09 11:19:46 -0400

    Added Day6

---
+++ Day6.py
@@ -0,0 +1,8 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == "Neel" and password == "1234":
+    print("Hello Neel")
+elif username == "James" and password == "1111":
+    print("Hello James")
+else:
+    print("Go away!")

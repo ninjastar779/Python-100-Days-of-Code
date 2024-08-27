8583aa0392bdfac4a9a2eb66f6c4f46a4222a7b6
ninjastar779 <neeldebnath2@gmail.com>
2024-08-27 17:19:49 -0400

    Added Day20

---
+++ Day20.py
@@ -0,0 +1,6 @@
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+increment = int(input("Enter increment: "))
+
+for i in range(start, end + 1, increment):
+    print(i)

e9d26cb1f838bb98e68e4d52564a8d1b606be94e
ninjastar779 <neeldebnath2@gmail.com>
2024-08-29 08:59:26 -0400

    Added Day31Project

---
+++ Day31Project.py
@@ -0,0 +1,11 @@
+def display_armbook_ui():
+    print("WELCOME TO")
+    print("\033[96m--    ARMBOK    --\033[0m")
+    print("\033[93mDefinitely not a rip off of\033[0m")
+    print("\033[93ma certain other social\033[0m")
+    print("\033[93mnetworking site.\033[0m")
+    print("\033[91mHonest.\033[0m\n")
+    print("Username: ")
+    print("Password: ")
+
+display_armbook_ui()

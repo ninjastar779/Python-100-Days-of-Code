9a14674fce63afbd718ca2e214d3479085b133fe
ninjastar779 <146141536+ninjastar779@users.noreply.github.com>
2024-09-14 22:11:49 +0000

    Added Day71.py

---
+++ Day71.py
@@ -0,0 +1,16 @@
+# from replit import db  #should theoretically work but won't due to Python version
+
+# print(db["neel"]["password"])
+# print(db["neel"]["salt"])
+# print("🌟Login System🌟")
+# username = input("Username > ")
+# password = input("Password > ")
+
+# salt = db[username]["salt"]
+# newPassword = f"{password}{salt}"
+# newPassword = hash(newPassword)
+# print(newPassword)
+# if newPassword == db["neel"]["password"]:
+#   print("Login successful")
+# else:
+#   print("Login denied")

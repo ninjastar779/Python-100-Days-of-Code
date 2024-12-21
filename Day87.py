3aefb3e15f74fd4e9573e233cb9b4656302e392c
ninjastar779 <146141536+ninjastar779@users.noreply.github.com>
2024-12-21 21:56:44 +0000

    Added Day87.py

--- Day87.py
+++ Day87.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello from Flask"
+    username = request.headers["X-Replit-User-Name"]
+    return f"Hello {username}"
 
-app.run(host="0.0.0.0", port=81)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
